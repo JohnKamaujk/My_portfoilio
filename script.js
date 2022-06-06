8ac56f1629820ec783c2328d22e3e4c32ea0177e
JohnKamaujk <johnjkkamau@gmail.com>
2022-06-06 09:51:42 +0300

    Edited the javascript file

---
+++ script.js
@@ -0,0 +1,6 @@
+// addded an eventlistener to te portfolio button ,so that it can always be directing to projects section
+const mainBtn = document.getElementById("portfolio");
+
+mainBtn.addEventListener("click",function(){
+  window.location.href("#projects")  
+})
